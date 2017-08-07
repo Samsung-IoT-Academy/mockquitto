ba93dd913687ff8e31e5a26b2a28380320820482
Georgiy Odisharia <math.kraut.cat@gmail.com>
2017-08-07 15:24:16 +0300

    Implemented GPS device, implemented base classes
    Signed-off-by: Georgiy Odisharia <math.kraut.cat@gmail.com>
    
    * Implemented base device class
    * GPS generator now works
    * Created class for different values
    * New wrapper for time-value pair
    * Generator base class has some new methods
    * Implemented some laws with random distribution independent from time

--- mockquitto/client/devices/device.py
+++ mockquitto/client/devices/device.py
@@ -1,8 +1,8 @@
 from abc import ABCMeta, abstractmethod
 import asyncio
 
-from mockquitto.client.exceptions import DeviceCreationError
 from mockquitto.client.generator import Generator
+
 
 class Device(metaclass=ABCMeta):
     """
@@ -18,36 +18,33 @@ class Device(metaclass=ABCMeta):
         iters -- number of iterations
     """
 
-    def __init__(self,
-                 format_str=None,
-                 generator: Generator=None,
+    def __init__(self, format_str=None, generator: Generator=None,
                  start_value=None, stop_value=None, iters=None):
         """
 
         :param format_str: string for formatting messages
-        :param generator: generator-like object
+        :param generator: object, which generates number
         :param start_value: starting value
         :param stop_value: stopping value
         :param iters: number of iterations
         """
         self._fmt_str = format_str
-        self._generator = generator
-
-        if gen_type is GenerationType.FINITE and (stop_value is None or iters is None):
-            raise DeviceCreationError(Msg="Finite device must have stop value or number of iterations!")
-        elif gen_type is GenerationType.INFINITE and stop_value is not None:
-            raise DeviceCreationError(Msg="Infinite device must not have stop value!")
+        self.generator = generator
 
         self._start_value = start_value
         self._stop_value = stop_value
         self._iter_nums = iters
 
-    @abstractmethod
-    @asyncio.coroutine
+        self._gen_obj = self._get_gen_obj()
+
+    def _get_gen_obj(self):
+        for value_pair in self.generator.get_gen_obj():
+            print(value_pair.time)
+            yield (value_pair.time, self.format_out(value_pair.value))
+
     def get(self):
-        for value_pair in self._generator.next():
-            yield (value_pair.time, self.format_out(value_pair.values))
+        return next(self._gen_obj)
 
     @abstractmethod
-    def format_out(self):
+    def format_out(self, value):
         pass
--- mockquitto/client/generator/generator.py
+++ mockquitto/client/generator/generator.py
@@ -7,7 +7,6 @@ from mockquitto.client.generator.laws import LawGeneration
 
 
 class FrequencyType(Enum):
-    _order_ = 'CONSTANT RANDOM'
     CONSTANT = 0
     RANDOM = 1
 
@@ -17,59 +16,109 @@ class GenerationType(Enum):
     INFINITE = 1
 
 
+class ValuePair:
+    """
+    Wrapper for return values
+
+    Attributes:
+        time: time for sleep (for asyncio event loop)
+        value: returning value
+    """
+    permissible_indexes = (0, 1)
+
+    def __init__(self, time, value):
+        self.time = time
+        self.value = value
+        self._dict = {
+            0: self.time,
+            1: self.value
+        }
+
+    def __getitem__(self, item):
+        if item not in self.permissible_indexes:
+            raise IndexError
+        elif not isinstance(item, int):
+            raise TypeError
+        else:
+            return self._dict.get(item)
+
+
 class Generator(metaclass=ABCMeta):
-    def __init__(self, start_value, gen_law: LawGeneration, freq_type: FrequencyType=FrequencyType.CONSTANT, **kwargs):
-        self._gen_law = gen_law
-        self._start_value = start_value
-        self._freq_type = freq_type
+    """
+    Base generator object for generating values
+    """
+    def __init__(self, start_value, gen_law,
+                 freq_type: FrequencyType=FrequencyType.CONSTANT, **kwargs):
+        """
+        Initializing Generator object
+
+        :param start_value:
+        :param gen_law: tuple, list of LawGeneration derived objects or one instance of it
+        :param freq_type:
+        :param kwargs:
+        """
+        self.gen_law_list = gen_law if isinstance(gen_law, (list, tuple)) else tuple(gen_law)
+        self.start_value = start_value
+        self.freq_type = freq_type
         if freq_type is FrequencyType.CONSTANT and 'freq_value' in kwargs:
-            self._freq_value = kwargs['freq_value']
+            self.freq_value = kwargs['freq_value']
         elif freq_type is FrequencyType.RANDOM and 'freq_range' in kwargs:
-            self._freq_range = kwargs['freq_range']
+            self.freq_range = kwargs['freq_range']
         else:
             raise GeneratorCreationError()
 
         self._generation_flag = False
+        self._generator_obj = self._generator_impl()
+        self._value_cls = self.start_value.__class__
 
     @abstractmethod
-    def next(self):
+    def _generator_impl(self):
         pass
 
-    def delay(self):
-        if self._freq_type is FrequencyType.CONSTANT:
-            return self._freq_value
-        elif self._freq_type is FrequencyType.RANDOM:
-            return random.uniform(*self._freq_range)
+    def get_gen_obj(self):
+        return self._generator_obj
 
-    def get_value_tuple(self):
-        return (self.delay(), self._gen_law.get())
+    def next(self):
+        return next(self._generator_obj)
+
+    def stop(self):
+        self._generation_flag = False
+
+    def delay(self):
+        if self.freq_type is FrequencyType.CONSTANT:
+            return self.freq_value
+        elif self.freq_type is FrequencyType.RANDOM:
+            return random.uniform(*self.freq_range)
+
+    def get_value_pair(self) -> ValuePair:
+        return ValuePair(self.delay(), self._value_cls([gen_law.get_next() for gen_law in self.gen_law_list]))
 
 
 class GeneratorFinite(Generator):
-    def __init__(self, start_value, gen_law: LawGeneration, *args, **kwargs):
+    def __init__(self, start_value, gen_law, *args, **kwargs):
         super().__init__(start_value, gen_law, *args, **kwargs)
         self._gen_type = GenerationType.FINITE
         self._stop_value = kwargs.get('stop_value')
         self._iters = kwargs.get('iters')
 
-    def next(self):
+    def _generator_impl(self):
         if self._stop_value:
-            value_tuple = self.get_value_tuple()
-            yield
-            while value_tuple[1] != self._stop_value and self._generation_flag:
-                value_tuple = self.get_value_tuple()
-                yield value_tuple
+            value = self.get_value_pair()
+            yield value[1]
+            while value[1] != self._stop_value and self._generation_flag:
+                value = self.get_value_pair()
+                yield value
         elif self._iters:
             for x in range(self._iters):
-                yield self.get_value_tuple()
+                yield self.get_value_pair()
 
 
 class GeneratorInfinite(Generator):
-    def __init__(self, start_value, gen_law: LawGeneration, *args, **kwargs):
+    def __init__(self, start_value, gen_law, *args, **kwargs):
         super().__init__(start_value, gen_law, *args, **kwargs)
         self._gen_type = GenerationType.INFINITE
 
-    def next(self):
+    def _generator_impl(self):
         self._generation_flag = True
         while self._generation_flag:
-            yield self.get_value_tuple()
+            yield self.get_value_pair()
